392618723da7a5397b3aea97f1efdc17543d2fec
Chip Carnes <chip.carnes27@gmail.com>
2016-07-15 10:23:04 -0700

    Add CRUD specs and methods for stylist

---
+++ lib/stylist.rb
@@ -0,0 +1,84 @@
+class Stylist
+  attr_reader(:name, :station, :id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @station = attributes.fetch(:station)
+    @id = attributes[:id]
+  end
+
+  define_method(:==) do |another_stylist|
+    self.name() == another_stylist.name() && self.station() == another_stylist.station()
+  end
+
+  define_singleton_method(:all) do
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch("name")
+      if name.include? "ß"
+        name = name.gsub(/ß/, "'")
+      end
+      station = stylist.fetch("station")
+      if station.include? "ß"
+        station = station.gsub(/ß/, "'")
+      end
+      id = stylist.fetch("id").to_i()
+      stylists.push(Stylist.new({name: name, station: station, id: id}))
+    end
+    stylists
+  end
+
+  define_method(:save) do
+    if @name.include? "'"
+      @name = @name.gsub(/'/, "ß")
+    end
+    if @station.include? "'"
+      @station = @station.gsub(/'/, "ß")
+    end
+    result = DB.exec("INSERT INTO stylists (name, station) VALUES ('#{@name}', '#{@station}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name, @name)
+    if @name.include? "'"
+      @name = @name.gsub(/'/, "ß")
+    end
+    @station = attributes.fetch(:name, @station)
+    if @station.include? "'"
+      @station = @station.gsub(/'/, "ß")
+    end
+    @id = self.id()
+    DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = '#{@id}';")
+    DB.exec("UPDATE stylists SET station = '#{@station}' WHERE id = '#{@id}';")
+  end
+
+  define_method(:delete) do
+    @id = self.id()
+    DB.exec("DELETE FROM stylists WHERE id = '#{@id}'")
+  end
+
+  define_singleton_method(:find) do |id|
+    found_stylist = nil
+    Stylist.all().each() do |stylist|
+      if stylist.id == id
+        found_stylist = stylist
+      end
+    end
+    found_stylist
+  end
+
+  define_method(:clients) do
+    stylist_clients = []
+    clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()} ORDER BY name ASC;")
+    clients.each() do |client|
+      name = client.fetch("name")
+      next_appointment = client.fetch("next_appointment")
+      stylist_id = client.fetch("stylist_id").to_i()
+      id = client.fetch("id").to_i()
+      specialty_clients.push(Client.new({name: name, next_appointment: next_appointment, stylist_id: stylist_id, id: id}))
+    end
+    stylist_clients
+  end
+end
---
+++ spec/stylist_spec.rb
@@ -0,0 +1,80 @@
+require('spec_helper')
+
+describe(Stylist) do
+  describe('#name') do
+    it("should return the name of the stylist") do
+      test_stylist = Stylist.new({name: "Mary Shelley", station: "1"})
+      expect(test_stylist.name()).to(eq("Mary Shelley"))
+    end
+  end
+
+  describe("#station") do
+    it("should return the station of the stylist") do
+      test_stylist = Stylist.new({name: "Mary Shelley", station: "1"})
+      expect(test_stylist.station()).to(eq("1"))
+    end
+  end
+
+  describe("#==") do
+    it("should be the same stylist if the name and station are the same") do
+      stylist1 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      stylist2 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+
+  describe(".all") do
+    it("should be empty at first") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("should add a stylist to the stylist database") do
+      test_stylist = Stylist.new({name: 'Mary Shelley', station: "1"})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe("#update") do
+    it("should update a stylists name and/or station") do
+      test_stylist = Stylist.new({name: 'Mary Shelley', station: "1"})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+      test_stylist.update({name: "Bob Ross", station: "2"})
+      expect(test_stylist.name()).to(eq("Bob Ross"))
+    end
+  end
+
+  describe("#delete") do
+    it("should delete the stylist from the database") do
+      stylist1 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      stylist1.save()
+      stylist2 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      stylist2.save()
+      stylist1.delete()
+      expect(Stylist.all()).to(eq([stylist2]))
+    end
+  end
+
+  describe(".find") do
+    it("should find a stylist by its id") do
+      stylist1 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      stylist1.save()
+      expect(Stylist.find(stylist1.id())).to(eq(stylist1))
+    end
+  end
+
+  describe("#clients") do
+    it("should return the list of cliests for a particular stylist") do
+      stylist1 = Stylist.new({name: 'Mary Shelley', station: "1"})
+      stylist1.save()
+      client1 = Client.new({name: "Joe Watts", next_appointment: "2016-09-09", stylist_id: stylist1.id(), id: nil})
+      client1.save()
+      client2 = Client.new({name: "Alison Hill", next_appointment: "2016-09-09", stylist_id: stylist1.id(), id: nil})
+      client2.save()
+      expect(stylist1.clients()).to(eq([client1, client2]))
+    end
+  end
+end
